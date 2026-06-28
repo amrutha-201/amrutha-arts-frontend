1a9b3b3ff6b9b2aaa0d2d629da013d808ed7ecb2
amrutha-201 <s210692@rguktong.ac.in>
2026-06-28 08:55:50 +0530

    Update AdminOrders.jsx

--- src/pages/AdminOrders.jsx
+++ src/pages/AdminOrders.jsx
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from'react';
-import {navigate} from "react-router-dom";
+import {Navigate} from "react-router-dom";
 import axios from'axios';
 function AdminOrders(){
     const[orders,setOrders]=useState([]);
